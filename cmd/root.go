014b37ba191ee12f6903e5654090e77da7f78f95
rkitamu <rkitamun@gmail.com>
2025-05-30 09:37:01 +0000

    feat(cmd): add Context struct for managing CLI arguments

--- cmd/root.go
+++ cmd/root.go
@@ -6,12 +6,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Context struct {
+	Args Args
+}
+
+type Args struct {
+	OutputFilePath string
+	Verbose        bool
+	Version        bool
+}
+
+var ctx Context
+
 var rootCmd = &cobra.Command{
 	Use:   "gomono",
 	Short: "gomono - Bundle local packages into a single file",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("test")
 	},
+}
+
+func init() {
+	ctx = Context{
+		Args: Args{},
+	}
+	// --output, -o
+	rootCmd.Flags().StringVarP(&ctx.Args.OutputFilePath, "output", "o", "", "Output file path (default: stdout)")
+
+	// --verbose, -v
+	rootCmd.Flags().BoolVarP(&ctx.Args.Verbose, "verbose", "v", false, "Enable verbose output")
+
+	// --version
+	rootCmd.Flags().BoolVarP(&ctx.Args.Version, "version", "", false, "Print version information")
 }
 
 func Execute() error {
